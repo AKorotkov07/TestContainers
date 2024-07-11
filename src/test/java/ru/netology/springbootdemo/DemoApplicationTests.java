fc76da617ea171547e54feadf69dffa67565914a
AKorotkov07 <153372700+AKorotkov07@users.noreply.github.com>
2024-07-11 22:02:20 +0300

    Add files via upload

---
+++ src/test/java/ru/netology/springbootdemo/DemoApplicationTests.java
@@ -0,0 +1,38 @@
+package ru.netology.springbootdemo;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DemoApplicationTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private static GenericContainer<?> devApp = new GenericContainer<>("dev-app-image").withExposedPorts(8080);
+    private static GenericContainer<?> prodApp = new GenericContainer<>("prod-app-image").withExposedPorts(8080);
+
+    @BeforeAll
+    public static void setUp() {
+        devApp.start();
+        prodApp.start();
+    }
+
+    @Test
+    void devProfileLoads() {
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + devApp.getMappedPort(8080) + "/profile", String.class);
+        assertEquals("Current profile is dev", response.getBody());
+    }
+
+    @Test
+    void prodProfileLoads() {
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + prodApp.getMappedPort(8080) + "/profile", String.class);
+        assertEquals("Current profile is production", response.getBody());
+    }
+}
